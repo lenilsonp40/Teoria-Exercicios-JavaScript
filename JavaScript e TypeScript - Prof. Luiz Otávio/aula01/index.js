6b0586e38cd3bedf308e7e20bf545b1df353f210
Lenilson Soares <lenilsonp40@gmail.com>
2022-06-21 11:19:53 -0300

    continua

--- JavaScript e TypeScript - Prof. Luiz Otávio/aula01/index.js
+++ JavaScript e TypeScript - Prof. Luiz Otávio/aula01/index.js
@@ -1,1 +1,7 @@
-console.log();
+console.log('Meu nome é "Lenilson". Estou aprendendo JavaScript às', 10, 'da manhã.');
+
+let nome = 'Lenilson'; // String
+
+console.log(nome,'nasceu em 1984.');
+console.log('Em 2000', nome , 'conheceu Maria.');
+console.log(nome,'casou-se com Maria em 2012.');
