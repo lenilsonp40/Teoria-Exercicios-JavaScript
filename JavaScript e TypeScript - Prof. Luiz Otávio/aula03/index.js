fa89e6e8dc9aecd9a79b5720989ddcf21f14f8e3
Lenilson Soares <lenilsonp40@gmail.com>
2022-06-23 10:43:52 -0300

    23/06/222

---
+++ JavaScript e TypeScript - Prof. Luiz Otávio/aula03/index.js
@@ -0,0 +1,9 @@
+window.confirm('Deseja realmente apagar?')
+
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite um numero');
+num1 = Number(num1);
+num2 = Number(num2);
+
+let res =  num1 + num2 ;
+window.alert(`O resultado é ${res}`)
