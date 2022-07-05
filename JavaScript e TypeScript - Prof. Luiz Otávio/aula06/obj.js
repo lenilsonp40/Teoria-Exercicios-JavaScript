b73bbc052254d66791baef03e7dfe6631b2b93be
Lenilson Soares <lenilsonp40@gmail.com>
2022-07-05 14:43:51 -0300

    05/06

---
+++ JavaScript e TypeScript - Prof. Luiz Otávio/aula06/obj.js
@@ -0,0 +1,11 @@
+function criapessoa (nome, sobrenome, idade) {
+    return {nome , sobrenome, idade};
+}
+
+const pessoa1 = criapessoa('Luiz', 'Miranda', 25);
+const pessoa2 = criapessoa('Maria','Oliveira', '32');
+
+console.log(pessoa1.nome, pessoa2.idade);
+
+
+dsdsds
