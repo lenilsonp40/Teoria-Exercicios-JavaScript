831c19d00c8ade4ced99033805ef86c06416370d
Lenilson Soares <lenilsonp40@gmail.com>
2022-07-20 16:05:24 -0300

    Update 01.js

--- Exercicios e desafios/Jessé/01.js
+++ Exercicios e desafios/Jessé/01.js
@@ -2,7 +2,7 @@
 Escreva um código que produz o seguinte:
 const array = [3, 78, 2, 16, 9]
 total de itens =  5
-soma - 108
+soma = 108
 numeros de pares ordenados : [2, 16 ,78]
 */ 
 
@@ -18,6 +18,7 @@ function numeros(num) {
 };
 console.log(`o total de itens é ${numeros.length}`) // Não conseguir trazer a quantidade de elementos desse array
 console.log(numeros([3, 78, 2, 16 ,9]));
+console.log( typeof numeros)
 
 
 
