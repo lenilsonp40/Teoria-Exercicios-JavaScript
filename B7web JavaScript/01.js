02c37776aa89163703632e1314b07ca84512786b
Lenilson Soares <lenilsonp40@gmail.com>
2022-11-07 15:04:47 -0300

    uptade

---
+++ B7web JavaScript/01.js
@@ -0,0 +1,16 @@
+/*
+let hora = 17;
+
+if (hora <= 12) {
+    console.log("Bom dia");
+} else if (hora < 18) {
+    console.log("Não é de manhã!");
+}else if ( hora <= 23) {
+    console.log("Boa noite");
+}
+*/
+
+
+
+
+
