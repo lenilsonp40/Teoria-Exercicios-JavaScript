97f4c9deaebc548092c3504aecf464b43c0fbd9c
Lenilson Soares <lenilsonp40@gmail.com>
2023-03-29 17:40:06 -0300

    up

---
+++ JavaScript Origamid/condicional/script.js
@@ -0,0 +1,11 @@
+let possuirGraduacao = false;
+let possuiDoutorado = true;
+
+if(possuirGraduacao) {
+    console.log('é verdadeiro');
+}else if(possuiDoutorado) {
+    console.log('é falso');
+}else {
+    console.log('Não Possui Nada!');
+}
+
--- JavaScript Origamid/variaveis/script.js
+++
@@ -1,5 +0,0 @@
-var nome = 'lenilson';
-console.log(typeof nome);
-
-var frase = "It's time"
-console.log(frase)
